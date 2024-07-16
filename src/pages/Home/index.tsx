28aaf1ff3fcfb122b276f29e68608372a6d58960
João Victor Mazetti Borges <jvmazettib@gmail.com>
2024-07-16 19:00:10 -0300

    correção cabeçalho

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import Footer from '../../Components/Footer'
-import Hero from '../../Components/Footer'
+import Hero from '../../Components/Hero'
 import Loader from '../../Components/Loader'
 import RestaurantList from '../../Components/RestaurantList'
 import { useGetRestaurantsQuery } from '../../services/api'
